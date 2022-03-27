30a05711b9f13471eccb42c9823e2bb9b7b314fb
ju-tilly <juce.duarte.f@gmail.com>
2022-03-27 20:53:01 -0300

    Class e Método de Mensagem criadas.

---
+++ src/br/com/juceduarte/mensagem/Mensagem.java
@@ -0,0 +1,11 @@
+package br.com.juceduarte.mensagem;
+
+public class Mensagem {
+    public static void main(String[] args) {
+        System.out.println("Exercício de mensagem");
+
+        MetodosMensagem.obterMensagem(7);
+        MetodosMensagem.obterMensagem(13);
+        MetodosMensagem.obterMensagem(20);
+    }
+}
---
+++ src/br/com/juceduarte/mensagem/MetodosMensagem.java
@@ -0,0 +1,25 @@
+package br.com.juceduarte.mensagem;
+
+public class MetodosMensagem {
+
+    public static void obterMensagem(int hora){
+        switch (hora) { //a partir do java 12 e superior esta é a nova escrita do CASE
+            case 5, 6, 7, 8, 9, 10, 11, 12 -> mensagemBomDia();
+            case 13, 14, 15, 16, 17 -> mensagemBoaTarde();
+            case 18, 19, 20, 21, 22, 23 -> mensagemBoaNoite();
+            default -> System.out.println("Hora inválida.");
+        }
+    }
+
+    private static void mensagemBoaNoite() {
+        System.out.println("Boa noite!!");
+    }
+
+    private static void mensagemBoaTarde() {
+        System.out.println("Boa tarde!!");
+    }
+
+    private static void mensagemBomDia() {
+        System.out.println("Bom dia!!");
+    }
+}
